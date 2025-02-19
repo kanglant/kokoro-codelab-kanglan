00df4b30d257a19b816b658baa927bdd5a1af080
Kanglan Tang <47437066+kanglant@users.noreply.github.com>
2025-02-19 13:22:18 -0800

    Test pyenv setup

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fail on any error.
-set -e
+# set -e
 
 # Display commands being run.
 # WARNING: please only enable 'set -x' if necessary for debugging, and be very
@@ -11,6 +11,16 @@ set -e
 #  Additionally, recursive invocation with credentials as command-line
 #  parameters, will print the full command, with credentials, in the build logs.
 # set -x
+set -exu -o history -o allexport
+
+# Install pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
+source ~/.bashrc
+pyenv install 3.10
+pyenv global 3.10
 
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
 # The final directory name in this path is determined by the scm name specified
