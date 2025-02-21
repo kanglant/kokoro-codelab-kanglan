115615ead231a0bf799e971ba8a8583feadf04f8
Kanglan Tang <47437066+kanglant@users.noreply.github.com>
2025-02-20 17:31:07 -0800

    Update kokoro_build.sh

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -14,6 +14,8 @@
 set -exu -o history -o allexport
 
 # Install pyenv
+python --version
+sudo apt install libssl-dev
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 pyenv_root=$HOME/.pyenv
 export PYENV_ROOT=$pyenv_root
@@ -21,9 +23,10 @@ export PATH="$HOME/.local/bin:$PYENV_ROOT/bin:$PATH"
 echo 'eval "$(pyenv init - bash)"' >> ~/.profile
 pyenv install 3.10
 pyenv global 3.10
+python --version
 
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
 # The final directory name in this path is determined by the scm name specified
 # in the job configuration.
-cd "${KOKORO_ARTIFACTS_DIR}/github/kokoro-codelab-kanglan"
-./build.sh
+# cd "${KOKORO_ARTIFACTS_DIR}/github/kokoro-codelab-kanglan"
+# ./build.sh
